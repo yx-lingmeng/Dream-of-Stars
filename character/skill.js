7cbea150f64257a067db39e9f8134ed63ee15c59
灰风 <133578152+YYUZDS@users.noreply.github.com>
2026-05-30 20:55:17 +0800

    Update skill.js

--- character/skill.js
+++ character/skill.js
@@ -25388,7 +25388,7 @@ const lmCharacter = {
 				if (player.hasSkill("dcboxuan")) {
 					player.storage.dcboxuan = true;
 				}
-				game.log(player, `修改了〖博玄〗`);
+				game.log(player, "修改了", "#g【博玄】");
 			},
 			ai: {
 				order: 5,
@@ -29922,7 +29922,7 @@ const lmCharacter = {
 		old_dcyizheng: "议政",
 		old_dcyizheng_info: "你的回合开始和结束时，你可与任意名其他角色各展示一张手牌，若展示的牌类型均相同，你可将这些牌交给一名角色，否则，你弃置这些牌。",
 		old_dcguilin: "归林",
-		old_dcguilin_info: "限定技，出牌阶段或你进入濒死状态时，你将体力回复至体力上限并摸回复值张牌，然后失去〖议政〗并修改〖博玄〗。",
+		old_dcguilin_info: `限定技，出牌阶段或你进入濒死状态时，你可以将体力回复至体力上限，然后失去${get.poptip("old_dcyizheng")}并修改${get.poptip("dcboxuan")}。`,
 		old_dc_zhugejun: "旧新杀诸葛均",
 		old_dc_zhugejun_prefix: "旧|新杀",
 		old_dcgumai: "孤脉",
